a2d5fbb3623a0792f5d865a45393f262f04561b8
Francios Delpech <delpechdefrayssinet.francois@gmail.com>
2020-08-23 21:31:17 -0400

    add calcul on bread

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { finalize } from 'rxjs/operators';
 
-import { QuoteService } from './quote.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { BreadService, BreadContext } from './bread.service';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -11,20 +13,28 @@ import { QuoteService } from './quote.service';
 export class HomeComponent implements OnInit {
   quote: string | undefined;
   isLoading = false;
+  breadForm: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder,
+    private modalController: ModalController,
+    private breadService: BreadService
+  ) {
+    this.breadForm = this.formBuilder.group({
+      floor: 400,
+      breadHumidity: 50,
+      saltPercent: 2.5,
+      levainHumidity: 60,
+      levainPercent: 40,
+    });
+  }
 
-  constructor(private quoteService: QuoteService) {}
-
-  ngOnInit() {
-    this.isLoading = true;
-    this.quoteService
-      .getRandomQuote({ category: 'dev' })
-      .pipe(
-        finalize(() => {
-          this.isLoading = false;
-        })
-      )
-      .subscribe((quote: string) => {
-        this.quote = quote;
-      });
+  ngOnInit() {}
+  async onSubmit(value: BreadContext) {
+    const result = this.breadService.calculate(value);
+    const modal = await this.modalController.create({
+      component: ModalPage,
+      componentProps: result,
+    });
+    return await modal.present();
   }
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
 import { IonicModule } from '@ionic/angular';
 import { Angulartics2Module } from 'angulartics2';
@@ -8,8 +9,7 @@ import { CoreModule } from '@core';
 import { SharedModule } from '@shared';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-import { QuoteService } from './quote.service';
-
+import { ResultBreatComponent } from './resultBread.component';
 @NgModule({
   imports: [
     CommonModule,
@@ -18,8 +18,10 @@ import { QuoteService } from './quote.service';
     SharedModule,
     IonicModule,
     Angulartics2Module,
+    ReactiveFormsModule,
     HomeRoutingModule,
   ],
   declarations: [HomeComponent],
+  entryComponents: [ResultBreatComponent],
 })
 export class HomeModule {}
